8aa6ec5d61edcf8cbf03cc3f0a4d93483bfe977c
WalterBlamer <wtblamer@gmail.com>
2026-03-20 10:42:05 -0500

    Pet files added & Pet.ts

---
+++ src/entities/Pet.ts
@@ -0,0 +1,31 @@
+import { BeforeInsert, Column, Entity, PrimaryColumn } from 'typeorm';
+import { v7 as uuidv7 } from 'uuid';
+
+@Entity()
+export class Pet {
+  @PrimaryColumn()
+  petId: string;
+
+  @BeforeInsert()
+  generateId(): void {
+    this.petId = uuidv7();
+  }
+
+  @Column()
+  petName: string;
+
+  @Column({ type: 'timestamptz' }) // stores current date
+  createdAt: Date;
+
+  @Column()
+  speciesId: number;
+
+  @Column()
+  outfitId: number;
+
+  @Column()
+  colorSchemeId: number;
+
+  @Column({ default: 50 })
+  happiness: number;
+}
