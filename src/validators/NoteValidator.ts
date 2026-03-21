034385836897acccd77d3194a3251e70e26494d9
Brianna Nava <bpn.nava@gmail.com>
2026-03-20 19:20:19 -0500

    NoteValidator

---
+++ src/validators/NoteValidator.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const CreateNoteSchema = z.object({
+  title: z.string().min(1).max(30),
+  text: z.string().min(1).max(160),
+});
+
+export type NoteBody = z.infer<typeof CreateNoteSchema>;
