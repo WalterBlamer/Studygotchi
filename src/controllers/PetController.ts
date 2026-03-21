1855cb81fbe938daac41e8c6067937f6890558bb
WalterBlamer <wtblamer@gmail.com>
2026-03-20 21:38:02 -0500

    createPet() function added

---
+++ src/controllers/PetController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { addPet } from '../models/PetModel.js';
+import { parseDatabaseError } from '../utils/db-utils.js';
+import { CreatePetSchema } from '../validators/PetValidator.js';
+
+async function createPet(req: Request, res: Response): Promise<void> {
+  const result = CreatePetSchema.safeParse(req.body);
+  if (!result.success) {
+    res.status(400).json(result.error.flatten());
+    return;
+  }
+
+  const { petName, outfitId, colorSchemeId, speciesId } = result.data;
+
+  try {
+    const newPet = await addPet(petName, outfitId, colorSchemeId, speciesId);
+    console.log(newPet);
+    res.sendStatus(201);
+  } catch (err) {
+    console.error(err);
+    const databaseErrorMessage = parseDatabaseError(err);
+    res.status(500).json(databaseErrorMessage);
+  }
+}
+
+export { createPet };
