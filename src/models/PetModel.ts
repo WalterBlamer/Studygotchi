b791e2f1b2c8a32a6e278c4d9ac2255bd0c85299
WalterBlamer <wtblamer@gmail.com>
2026-03-20 21:14:46 -0500

    PetModel.ts written

---
+++ src/models/PetModel.ts
@@ -0,0 +1,25 @@
+import { AppDataSource } from '../dataSource.js';
+import { Pet } from '../entities/Pet.js';
+
+const petRepository = AppDataSource.getRepository(Pet);
+
+async function addPet(
+  petName: string,
+  outfitId: number,
+  colorSchemeId: number,
+  speciesId: number,
+): Promise<Pet> {
+  const newPet = new Pet();
+  newPet.petName = petName;
+  newPet.outfitId = outfitId;
+  newPet.colorSchemeId = colorSchemeId;
+  newPet.speciesId = speciesId;
+
+  return petRepository.save(newPet);
+}
+
+async function getPetById(petId: string): Promise<Pet | null> {
+  return petRepository.findOne({ where: { petId } });
+}
+
+export { addPet, getPetById };
