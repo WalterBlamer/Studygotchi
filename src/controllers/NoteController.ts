82519bb8740cebcdde4ff8cd12db4f8e412caa12
Brianna Nava <bpn.nava@gmail.com>
2026-03-20 21:22:19 -0500

    NoteController.ts + NoteModel fixes

---
+++ src/controllers/NoteController.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from "express";
+import { CreateNoteSchema } from "../validators/NoteValidator.js";
+import { NoteModel } from "../models/NoteModel.js";
+import { parseDatabaseError } from "../utils/db-utils.js";
+
+async function createNote(req: Request, res: Response): Promise<void> {
+  const result = CreateNoteSchema.safeParse(req.body);
+
+  if (!result.success) {
+    res.status(400).json({ errors: result.error });
+    return;
+  }
+
+  const { title, text } = result.data;
+
+  try {
+    const newNote = await addNote(title, text);
+    console.log(newNote);
+    res.sendStatus(201).json(newNote);
+  } catch (err) {
+    console.error(err);
+    const databaseErrorMessage = parseDatabaseError(err);
+    res.status(500).json(databaseErrorMessage);
+  }
+}
+
+async function getNotes(req: Request, res: Response): Promise<void> {
+  try {
+    const notes = await getNotes();
+    res.sendStatus(201).json(notes);
+  } catch (err) {
+    console.error(err);
+    const databaseErrorMessage = parseDatabaseError(err);
+    res.status(500).json(databaseErrorMessage);
+  }
+}
+
+async function getByTitle(req: Request, res: Response): Promise<void> {
+  const title = req.params.title;
+
+  if (!title) {
+    res.status(400).json({ errors: "Note title required." });
+    return;
+  }
+
+  try {
+    const note = await getByTitle(title);
+    if (!note) {
+      res.status(404).json({ errors: "Note not found." });
+      return;
+    }
+    res.sendStatus(200).json(note);
+  } catch (err) {
+    console.error(err);
+    const databaseErrorMessage = parseDatabaseError(err);
+    res.status(500).json(databaseErrorMessage);
+  }
+}
+
+export { createNote, getNotes, getByTitle };
--- src/models/NoteModel.ts
+++ src/models/NoteModel.ts
@@ -7,15 +7,15 @@ async function getAllNotes(): Promise<Note[]> {
   return await noteRepository.find();
 }
 
-async function getNoteById(NoteId: string): Promise<Note | null> {
-  return await noteRepository.findOne({ where: { noteId } });
+async function getNoteByTitle(title: string): Promise<Note | null> {
+  return await noteRepository.findOne({ where: { title } });
 }
 
 async function createNote(title: string, text: string): Promise<Note> {
   const newNote = new Notes();
   newNote.title = title;
   newNote.text = text;
-  return await noteRepository.save(newNote);
+  return await noteRepository.save(note);
 }
 
-export { getAllNotes, getNoteById };
+export { getAllNotes, getNoteByTitle, createNote };
