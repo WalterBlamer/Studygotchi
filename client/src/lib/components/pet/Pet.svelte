3840de3e5480235ccb0f5d5b65cb5d5be03dffad
BNava7 <bpn.nava@gmail.com>
2026-05-07 09:22:06 -0500

    Added Pet frontend stuff, images and svelte page

---
+++ client/src/lib/components/pet/Pet.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let pet;
+</script>
+
+<div class="pet-container">
+  <img class="pet-image" src={`/pets/species-${pet.speciesId}.png`} alt={pet.petName} />
+  <p>{pet.petName}</p>
+</div>
+
+<style>
+  .pet-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .pet-image {
+    width: 120px;
+    height: 120px;
+    image-rendering: pixelated;
+  }
+</style>
---
+++ client/src/routes/create-pet/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { createPet } from '$lib/services/petService';
+
+  let petName = '';
+  let speciesId = 1;
+
+  async function handleSubmit() {
+    await createPet({
+      petName,
+      speciesId,
+    });
+  }
+</script>
+
+<input bind:value={petName} placeholder="Pet name" />
+
+<select bind:value={speciesId}>
+  <option value={1}>Dog</option>
+  <option value={2}>Llama</option>
+</select>
+
+<button on:click={handleSubmit}> Create Pet </button>
